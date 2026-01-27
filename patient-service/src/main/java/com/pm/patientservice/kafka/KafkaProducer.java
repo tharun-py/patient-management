9c8fa2b1ccfae824128fe3cdbe961f55640056e2
tharun-py <81326031+tharun-py@users.noreply.github.com>
2026-01-26 20:31:58 -0600

    Add analytics-service with Kafka, fix Spring Boot compatibility issues

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,35 @@
+package com.pm.patientservice.kafka;
+
+import com.pm.patientservice.model.Patient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Slf4j
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+
+        PatientEvent event = PatientEvent.newBuilder().setPatientId(patient.getId().toString())
+                .setName(patient.getName()).setEmail(patient.getEmail()).setEventType("PATIENT_CREATED").build();
+
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+
+        }catch (Exception e){
+            log.error("Error sending Patient Created Event: {}", event);
+        }
+    }
+
+}
+
+
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -17,13 +18,14 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
+    private final KafkaProducer kafkaProducer;
     private PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
-
+        this.kafkaProducer = kafkaProducer;
     }
 
 
@@ -47,6 +49,7 @@ public class PatientService {
 
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
 
+        kafkaProducer.sendEvent(newPatient);
         return PatientMapper.toDTO(newPatient);
     }
 
